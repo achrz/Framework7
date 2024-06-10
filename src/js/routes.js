51a59c63737326e60a4c4141f2555e5a95e570c9
Achmad Rizky <achmadrizky486@gmail.com>
2024-06-11 05:35:08 +0700

    feat : Latihan 5

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,10 +1,20 @@
 
 import HomePage from '../pages/home.f7';
+import Matematika from '../pages/matematika.f7';
+import Diskon from '../pages/diskon.f7';
 
 var routes = [
   {
     path: '/',
     component: HomePage,
+  },
+  {
+    path: '/matematika/',
+    component: Matematika,
+  },
+  {
+    path: '/diskon/',
+    component: Diskon,
   },
 ];
 
